c4e2ba927146202f3abd657bf2954ad33bd15942
Andrei <andrei.drews@yahoo.com>
2025-10-21 11:33:10 +0300

    FIX: Set dark blue #090A1A background everywhere - homepage, evenimente, all pages

--- resources/views/acasa/evenimente.blade.php
+++ resources/views/acasa/evenimente.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('title', 'Evenimente – Auditie Placuta')
+@section('body_class', 'page-evenimente')
+
+@push('styles')
+  <link rel="stylesheet" href="{{ asset('assets/css/evenimente.css') }}?v={{ filemtime(public_path('assets/css/evenimente.css')) }}">
+@endpush
 
 @section('content')
     @include('components.in-constructie')
